0239bba755f19df2b726732f7d2ec1cbae539377
DanielRochaOdo <daniel.rocha@odontoart.com>
2025-06-27 14:36:03 -0300

    Updated FileUpload.tsx

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -55,7 +55,8 @@ const FileUpload: React.FC<FileUploadProps> = ({ onSuccess, onCancel }) => {
     const data: ParsedData[] = [];
 
     for (let i = 1; i < lines.length; i++) {
-      const values = lines[i].split(',').map(v => v.trim().replace(/^"|"$/g, ''));
+      const headers = lines[0].split(',').map(h => h.trim().toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, ''));
+
       const row: any = {};
 
       headers.forEach((header, index) => {
