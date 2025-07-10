81c4b911fe44365c04d7211902ead2dcb4ea2991
DanielRochaOdo <daniel.rocha@odontoart.com>
2025-07-10 12:00:31 -0300

    Updated Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -111,7 +111,7 @@ const Login: React.FC = () => {
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                   className="block w-full pl-9 sm:pl-10 pr-3 py-2 sm:py-3 border border-neutral-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary-500 focus:border-transparent transition-colors duration-200 text-sm sm:text-base"
-                  placeholder="admin@serverkey.com"
+                  placeholder="insira um e-mail válido"
                   disabled={loading}
                 />
               </div>
